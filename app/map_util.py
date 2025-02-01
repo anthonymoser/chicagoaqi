17caf7242288af514e8c6eed093279f2de27bc84
Anthony Moser <anthony.moser@gmail.com>
2025-01-31 23:19:21 -0600

    chinese language support

--- app/map_util.py
+++ app/map_util.py
@@ -57,6 +57,42 @@ map_layer_config = {
     )
 }
 
+
+
+
+map_layer_config_chinese = {
+    "corridors": MapLayer(
+        label = "工业区域",
+        layer_type = "bounds",
+        point_label = "工业区域",
+        filename = "Boundaries - Industrial Corridors (current).geojson",
+        style =  {
+                    "color": "grey", 
+                    "weight":.75,
+                    "fillOpacity": 0.5
+                }
+    ), 
+    "communities": MapLayer(
+        label = " 邻里界线", 
+        layer_type = "bounds",
+        point_label = " 邻里界线",
+        filename = "Boundaries - Community Areas (current).geojson",
+        style =  {
+            "color": "grey", 
+            "fillOpacity": 0,
+            "weight": 0.6
+        }
+    ),
+    "tracts": MapLayer(
+        label = "人口普查调查区", 
+        layer_type = "bounds",
+        point_label = "人口普查调查区",
+        filename = "ej_index.geojson",
+        style =  {}
+    )
+}
+
+
 def locate_point(lat, long, bounds):
     marker = gpd.points_from_xy(x=long, y=lat, crs="EPSG:4326")
     mf = gpd.GeoDataFrame(marker).set_geometry(0).rename_geometry('geometry')
